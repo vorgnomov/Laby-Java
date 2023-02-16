d1db55303372408aa9f817ba3e353035daf33004
Vorgnomov <Sakolinskayaveronika@mail.ru>
2023-02-16 09:26:15 +0300

    fix lr2

--- src/LR2/zadanie2.java
+++ src/LR2/zadanie2.java
@@ -15,8 +15,8 @@ public class zadanie2 {
         int sum = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                mas[i][j]= random.nextInt();
-                //mas[i][j] = random.nextInt(-10, 10);
+                int i1 = -10;
+                mas[i][j]= (int) ((Math.random())*((10-i1)+1))+i1;
                 System.out.print(mas[i][j] + "  ");
             }
             System.out.println();
